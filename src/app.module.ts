2ea72a492f4ca959e85a422ca6d4054537cd8f86
black-adm <mattheuscontato17@gmail.com>
2023-11-01 20:37:41 -0300

    add entitie Notification and folders structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { PrismaService } from './prisma.service';
+import { AppController } from './infra/app.controller';
+import { HttpModule } from './infra/http.module';
+import { PrismaService } from './infra/prisma.service';
 
 @Module({
-  imports: [],
+  imports: [HttpModule],
   controllers: [AppController],
-  providers: [
-    PrismaService
-  ],
+  providers: [PrismaService],
 })
 export class AppModule { }
