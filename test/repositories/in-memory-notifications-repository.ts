dc1ecb27df5bad2d03c01e48376634d12216f82c
black-adm <mattheuscontato17@gmail.com>
2023-11-02 02:01:55 -0300

    update repositorys for implements new methods

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -3,8 +3,16 @@ import { Notification } from "@application/entities/notification";
 
 export class InMemoryNotificationsRepository implements NotificationsRepository {
     public notifications: Notification[] = [];
-    
+
+    async findById(notificationId: string): Promise<Notification | null> {
+        throw new Error("Method not implemented.");
+    }
+
     async create(notification: Notification) {
         this.notifications.push(notification);
+    }
+
+    async save(notification: Notification): Promise<void> {
+        throw new Error("Method not implemented.");
     }
 };
