ebac2938771184f047cea8aa426c36a03797d9a4
black-adm <mattheuscontato17@gmail.com>
2023-11-01 22:05:56 -0300

    correction from notification test for  simulated database

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,8 +1,15 @@
+import { Notification } from "../entities/notification";
 import { SendNotification } from "./send-notification";
+
+const notificationsRepository = {
+    async create(notification: Notification) {
+        console.log(notification);
+    },
+};
 
 describe('Send notification', () => {
     test('Deve ser capaz de enviar uma notificação.', async () => {
-        const sendNotification = new SendNotification();
+        const sendNotification = new SendNotification(notificationsRepository);
 
         const { notification } = await sendNotification.execute({
             content: 'Ajuste nos valores mensais dos servidores.',
