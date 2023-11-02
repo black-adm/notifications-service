418fbaded9c0068c1e5ad3461fec1bf0bd43fe5e
black-adm <mattheuscontato17@gmail.com>
2023-11-01 22:31:32 -0300

    concept from in memory database and notification test file update

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,14 +1,9 @@
-import { Notification } from "../entities/notification";
+import { InMemoryNotificationsRepository } from "../../../test/repositories/in-memory-notifications-repository";
 import { SendNotification } from "./send-notification";
-
-const notificationsRepository = {
-    async create(notification: Notification) {
-        console.log(notification);
-    },
-};
 
 describe('Send notification', () => {
     test('Deve ser capaz de enviar uma notificação.', async () => {
+        const notificationsRepository = new InMemoryNotificationsRepository();
         const sendNotification = new SendNotification(notificationsRepository);
 
         const { notification } = await sendNotification.execute({
@@ -17,6 +12,7 @@ describe('Send notification', () => {
             recipientId: 'recip-2023-11-01-345'            
         });
 
-        expect(notification).toBeTruthy();
+        expect(notificationsRepository.notifications).toHaveLength(1);
+        expect(notificationsRepository.notifications[0]).toEqual(notification);
     });
 });
