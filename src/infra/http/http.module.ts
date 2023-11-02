703d170a4b97d7da36742c16c25a7a74676119ea
black-adm <mattheuscontato17@gmail.com>
2023-11-02 14:10:27 -0300

    finished update http module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,12 +1,25 @@
 import { Module } from '@nestjs/common'
 import { DatabaseModule } from '../database/database.module';
 import { NotificationsController } from './controllers/notifcations.controller';
+
 import { SendNotification } from '@application/use-cases/send-notification';
+import { GetRecipientNotifications } from '@application/use-cases/get-recipient-notifications';
+import { CountRecipientNotifications } from '@application/use-cases/count-recipient-notifications';
+import { ReadNotification } from '@application/use-cases/read-notification';
+import { UnreadNotification } from '@application/use-cases/unread-notification';
+import { CancelNotification } from '@application/use-cases/cancel-notificaton';
 
 @Module({
     imports: [DatabaseModule],
     controllers: [NotificationsController],
-    providers: [SendNotification],
+    providers: [
+        SendNotification,
+        GetRecipientNotifications,
+        CountRecipientNotifications,
+        ReadNotification,
+        UnreadNotification,
+        CancelNotification
+    ],
 })
 
 export class HttpModule {};
